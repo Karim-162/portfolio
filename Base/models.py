5a940f8d0246b5cfb3f92db327fdf371832fbf42
Karim-162 <shafayetkarim04@gmail.com>
2026-07-03 12:44:30 +0600

    i worked on project section to add projects from backend
    i worked on project section to add projects from backend

--- Base/models.py
+++ Base/models.py
@@ -8,4 +8,13 @@ class Contact(models.Model):
     content=models.TextField(max_length=400)
     number=models.CharField(max_length=11)
 
+class Project(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    github_link = models.URLField(blank=True)
+    live_link = models.URLField(blank=True)
+
+    def __str__(self):
+        return self.title
+
 #so models has been created now we have to register this models in admin
--- Base/views.py
+++ Base/views.py
@@ -1,44 +1,95 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.contrib import messages
-from Base import models
-from Base.models import Contact
+# from django.shortcuts import render
+# from django.http import HttpResponse
+# from django.contrib import messages
+# from Base import models
+# from Base.models import Contact,Project
 
-# def home(request):
+
+# # def home(request):
+# #     return render(request,'home.html')
+# def contact(request):
+#     if request.method=="POST":
+#         print('post')
+#         name=request.POST.get('name')
+#         email=request.POST.get('email')
+#         number=request.POST.get('number')
+#         content=request.POST.get('content')
+
+#         print(name,email,number,content)
+
+#         if len(name)>1 and len(name)<30:
+#             pass
+#         else:
+#             messages.error(request,'length of the name should be greater than 1 and less than 30')
+#             return render(request,'home.html')
+        
+#         if len(email)>1 and len(email)<30:
+#             pass
+#         else:
+#             messages.error(request,'length of the email should be greater than 1 and less than 30')
+#             return render(request,'home.html')
+        
+#         if len(number)>1 and len(number)<12:
+#             pass
+#         else:
+#             messages.error(request,'length of the number should be greater than 1 and less than 12')
+#             return render(request,'home.html')
+        
+#         ins=models.Contact(name=name,email=email,number=number,content=content)
+
+#         ins.save()
+
+#         messages.success(request,'Thank you for contacting me || Your response has been saved')
+
+#         print('data has been saved into database')
+
+#         projects = Project.objects.all()
+
+#         context = {
+#             "projects": projects,
+#         }
 #     return render(request,'home.html')
+
+
+from django.shortcuts import render
+from django.contrib import messages
+from Base.models import Contact, Project
+
+
 def contact(request):
-    if request.method=="POST":
-        print('post')
-        name=request.POST.get('name')
-        email=request.POST.get('email')
-        number=request.POST.get('number')
-        content=request.POST.get('content')
+    # সবসময় Project গুলো নিয়ে আসবে
+    projects = Project.objects.all()
 
-        print(name,email,number,content)
+    if request.method == "POST":
+        print("POST")
 
-        if len(name)>1 and len(name)<30:
-            pass
+        name = request.POST.get("name")
+        email = request.POST.get("email")
+        number = request.POST.get("number")
+        content = request.POST.get("content")
+
+        if not (1 < len(name) < 30):
+            messages.error(request, "Length of the name should be greater than 1 and less than 30")
+
+        elif not (1 < len(email) < 30):
+            messages.error(request, "Length of the email should be greater than 1 and less than 30")
+
+        elif not (1 < len(number) < 12):
+            messages.error(request, "Length of the number should be greater than 1 and less than 12")
+
         else:
-            messages.error(request,'length of the name should be greater than 1 and less than 30')
-            return render(request,'home.html')
-        
-        if len(email)>1 and len(email)<30:
-            pass
-        else:
-            messages.error(request,'length of the email should be greater than 1 and less than 30')
-            return render(request,'home.html')
-        
-        if len(number)>1 and len(number)<12:
-            pass
-        else:
-            messages.error(request,'length of the number should be greater than 1 and less than 12')
-            return render(request,'home.html')
-        
-        ins=models.Contact(name=name,email=email,number=number,content=content)
+            contact = Contact(
+                name=name,
+                email=email,
+                number=number,
+                content=content
+            )
+            contact.save()
 
-        ins.save()
+            messages.success(request, "Thank you for contacting me. Your response has been saved.")
 
-        messages.success(request,'Thank you for contacting me || Your response has been saved')
+    context = {
+        "projects": projects,
+    }
 
-        print('data has been saved into database')
-    return render(request,'home.html')
+    return render(request, "home.html", context)
